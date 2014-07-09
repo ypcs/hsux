8519f0b028c9973aa876e1f316a4c41ff294143e
Ville Korhonen <ville@xd.fi>
2014-07-09 22:07:01 +0300

    Added more logging.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,4 +1,4 @@
-//example of using a message handler from the inject scripts
+console.log("Loading background.js");
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
   	chrome.pageAction.show(sender.tab.id);
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,4 @@
+console.log("Loading inject.js.");
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -7,7 +8,10 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-
+        // TODO: Check if body has class (full page add, requires user 
+        //       intervention) & user is logged in (ie. has probably 
+        //       paid for content)
+        // user-logged-in ; frontpage-ad-visible
 	}
 	}, 10);
 });
