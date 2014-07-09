11782a971f18775b5788f790b9e3de7b8278ce92
Ville Korhonen <ville@xd.fi>
2014-07-09 22:18:02 +0300

    Removed boilerplate code.

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,6 +1,1 @@
 console.log("Loading background.js");
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
