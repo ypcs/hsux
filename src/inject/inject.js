f2ae5ea3819c60651f9567ad11779de6f90e13d0
Ville Korhonen <ville@xd.fi>
2016-11-02 15:00:21 +0200

    Fix indentation

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,12 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+       if (document.readyState === "complete") {
+          clearInterval(readyStateCheckInterval);
 
-        var doc_body = document.getElementsByTagName('body');
-        
-        if (doc_body.length != 1) {return;}
-        
+          var doc_body = document.getElementsByTagName('body');
+          
+          if (doc_body.length != 1) {return;}
+          
         // If full page ad is displayed & user is logged in, remove ad
         if (doc_body[0].classList.contains('frontpage-ad-visible') && doc_body[0].classList.contains('user-logged-in')) {
             console.log("HSUX: Removing full-page ad.");
@@ -19,14 +19,14 @@ chrome.extension.sendMessage({}, function(response) {
             //'news-list-area',         // Newsfeeds for IL, Metro, etc.
             //'iltapulu_fp_box_iframe', // Iltapulu
             //'top-extra-links',        // Top extra links, 'HS', 'Nyt', 'Oikotie' etc.
-        ];
+            ];
 
-        for (var i=0; i<hide_these_elements.length; i++) {
-            var e = document.getElementById(hide_these_elements[i]);
-            if (e) {
-                e.remove();
+            for (var i=0; i<hide_these_elements.length; i++) {
+                var e = document.getElementById(hide_these_elements[i]);
+                if (e) {
+                    e.remove();
+                }
             }
         }
-	}
-	}, 10);
+    }, 10);
 });
