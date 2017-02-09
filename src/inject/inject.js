52582ff8079210732fe4e5439ef6b902aafbe43b
Ville Korhonen <ville@xd.fi>
2017-02-09 09:38:46 +0200

    Enable analytics events

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,12 @@
 chrome.extension.sendMessage({}, function(response) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-420043-34', 'auto', 'hsux');
+    ga('hsux.send', {hitType: 'event', eventCategory: 'user', eventAction: 'load', eventLabel: 'Load HSUX'});
+
 	var readyStateCheckInterval = setInterval(function() {
        if (document.readyState === "complete") {
           clearInterval(readyStateCheckInterval);
@@ -11,6 +19,7 @@ chrome.extension.sendMessage({}, function(response) {
         if (doc_body[0].classList.contains('frontpage-ad-visible') && doc_body[0].classList.contains('user-logged-in')) {
             console.log("HSUX: Removing full-page ad.");
             doc_body[0].classList.remove('frontpage-ad-visible');
+            ga('hsux.send', {hitType: 'event', eventCategory: 'user', eventAction: 'remove-frontpage-ad', eventLabel: 'Remove frontpage ad'});
         }
 
         // Remove other elements from the page
@@ -26,6 +35,7 @@ chrome.extension.sendMessage({}, function(response) {
                 if (e) {
                     console.log("HSUX: removing element #" + hide_these_elements[i] + ".");
                     e.remove();
+                    ga('hsux.send', {hitType: 'event', eventCategory: 'user', eventAction: 'remove-element-' + hide_these_elements[i], eventLabel: 'Remove element ' + hide_these_elements[i]});
                 }
             }
         }
